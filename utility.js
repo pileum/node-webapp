1bf70d30bf05835120901a108d2e9dd9674afbb1
Pilleum <81552852+pileum@users.noreply.github.com>
2022-08-29 18:04:41 +0700

    Add files via upload

--- utility.js
+++ utility.js
@@ -2,7 +2,7 @@
 
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5MTczODQ5LCJleHAiOjE2NTkxNzc0NDksIm5iZiI6MTY1OTE3Mzg0OSwianRpIjoibE1yNmxEdmVzcU5vcmhVNyIsInN1YiI6MTYsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.M_yG8XICrHAEVM-nCE68JKzb95dvUxceL1sPo3WnLgs"; // put access token
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzY4NjI2LCJleHAiOjE2NjE3NzIyMjYsIm5iZiI6MTY2MTc2ODYyNiwianRpIjoidTlrUGRjZVFUcE1PalI2RSIsInN1YiI6MTYsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.pgpFJQOMC4OLoEwleZ8BwVr3D3WIfyKRGX4wXbb5S4w"; // put access token
 const student = {
   name: '', // replace with your full name.
   age: 19, // put your age.
